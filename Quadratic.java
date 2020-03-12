3d49a8ac909ad4fb5cec80547e7a6460b0183de8
swdmahesh <62048317+swdmahesh@users.noreply.github.com>
2020-03-12 16:26:14 +0530

    Update Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -1,3 +1,11 @@
+/*Write a program Quadratic.java to find the roots of the equation 
+a*x*x + b*x + c. Since the equation is x*x, hence there are 2 roots. 
+The 2 roots of the equation can be found using a formula (Note: Take a, b and c as input values)
+delta = b*b - 4*a*c
+Root 1 of x = (-b + sqrt(delta))/(2*a)
+Root 2 of x = (-b - sqrt(delta))/(2*a)
+*/
+
 import java.lang.Math;
 import java.util.Scanner;
 public class Quadratic
